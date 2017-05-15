a9405a5c0c4edf546c02f22117761ca6c1b71c6a
serefaltindal <serefaltindal@gmail.com>
2017-05-15 21:08:06 +0300

    first commit

---
+++ stream/src/main/java/stream/Run.java
@@ -0,0 +1,11 @@
+package stream;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Stream8 git repository control");
+	}
+
+}
